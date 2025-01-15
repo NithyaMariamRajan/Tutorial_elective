c016eddc998d6f8cb40f2acef9f95aa6d3fb3c09
Nithya Mariam Rajan <166158985+NithyaMariamRajan@users.noreply.github.com>
2025-01-15 21:08:51 +0530

    Update income tax using if-elif.py

--- income tax using if-elif.py
+++ income tax using if-elif.py
@@ -1,22 +1,15 @@
+print("...Calculating income tax...\n")
 
-def calculate_tax(income):
+income = float(input("Enter your annual income (\u20b9): "))
+tax=0
+if income <= 250000:
     tax = 0
-    if income <= 250000:
-        tax = 0
-    elif 250001 <= income <= 500000:
-        tax = (income - 250000) * 0.05
-    elif 500001 <= income <= 1000000:
-        tax = (income - 500000) * 0.2 + 250000 * 0.05
-    else:
-        tax = (income - 1000000) * 0.3 + 500000 * 0.2 + 250000 * 0.05
-    return tax
+elif income <= 500000:
+    tax = (income - 250000) * 0.05
+elif income <= 1000000:
+    tax = (income - 500000) * 0.2 + 250000 * 0.05
+else:
+    tax = (income - 1000000) * 0.3 + 500000 * 0.2 + 250000 * 0.05
 
-try:
-    print("...Calculating income tax...\n")  
-    
-    income = float(input("Enter your annual income (₹): "))
-    
-    tax_amount = calculate_tax(income)
-    print(f"\nYour calculated tax is.... ₹{tax_amount:.2f}")
-except ValueError:
-    print("Invalid input! Please enter a numeric value.")
+print("Total tax_amount is...:",tax)
+
