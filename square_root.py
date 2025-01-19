dca51ca5d67717a3f8286dee9e11a5c410a17241
Nithya Mariam Rajan <166158985+NithyaMariamRajan@users.noreply.github.com>
2025-01-19 18:02:07 +0530

    square_root

--- square_root.py
+++ square_root.py
@@ -1,5 +1,4 @@
 //to find the square_root of a number
-
-n = int(input("Enter the number:")) 
+n = int(input("Enter the number: ")) 
 square_root = n ** 0.5  
 print("Square root of", n, "is", square_root)
