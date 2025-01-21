5ed49e79ca2db0fb62570b142df9bcf68f2f88a2
Nithya Mariam Rajan <166158985+NithyaMariamRajan@users.noreply.github.com>
2025-01-21 10:45:59 +0530

    income_tax

--- income_tax.py
+++ income_tax.py
@@ -1,14 +1,17 @@
-print("...Calculating income tax...\n")
 
-income = float(input("Enter your annual income (\u20b9): "))
-tax=0
-if income <= 250000:
+salary = float(input("enter salary: "))
+
+if salary <=300000:
     tax = 0
-elif income <= 500000:
-    tax = (income - 250000) * 0.05
-elif income <= 1000000:
-    tax = (income - 500000) * 0.2 + 250000 * 0.05
+elif salary <=700000:
+    tax = (salary-300000)*0.05
+elif salary <= 1000000:
+        tax = (400000 * 0.05) + (salary - 700000) * 0.1
+elif salary <= 1200000:
+        tax = (400000 * 0.05) + (300000 * 0.1) + (salary - 1000000) * 0.15   
+elif salary <= 1500000:
+        tax = (400000 * 0.05) + (300000 * 0.1) + (200000 * 0.15) + (salary - 1200000) * 0.2   
 else:
-    tax = (income - 1000000) * 0.3 + 500000 * 0.2 + 250000 * 0.05
+        tax = (400000 * 0.05) + (300000 * 0.1) + (200000 * 0.15) + (300000 * 0.2) + (salary - 1500000) * 0.3
 
-print("Total tax_amount is...:",tax)
+print("Total tax amount: ", tax)
